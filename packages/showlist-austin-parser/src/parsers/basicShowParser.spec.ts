ecf4cc4120fc44cd25b13b494646a1aff85e23ba
Brandon Valosek <bvalosek@gmail.com>
2018-11-26 12:05:06 -0600

    reorg tests to be more jestful

--- packages/showlist-austin-parser/src/parsers/basicShowParser.spec.ts
+++ packages/showlist-austin-parser/src/parsers/basicShowParser.spec.ts
@@ -2,28 +2,32 @@ import { basicShowParser } from './basicShowParser';
 import moment = require('moment-timezone');
 import { Show } from '../types';
 
-test('basicShowParser: Simple show', () => {
+describe('basicShowParser', () => {
 
-  const html = `
-    The Tragically Hip at <a href="http://www.emosaustin.com"><b>Emo&apos;s</b></a> (2015 E. Riverside) [<a href="cgi/genpage.cgi?venue=emos" title="list by venue">+</a>] <font color="#666666">[6:30pm doors, 8pm show]</font>
-  `;
+  it('should parse a simple show', () => {
 
-  const shows: Show[] = [];
-  basicShowParser(html, shows, moment());
-  const [ show ] = shows;
+    const html = `
+      The Tragically Hip at <a href="http://www.emosaustin.com"><b>Emo&apos;s</b></a> (2015 E. Riverside) [<a href="cgi/genpage.cgi?venue=emos" title="list by venue">+</a>] <font color="#666666">[6:30pm doors, 8pm show]</font>
+    `;
 
-  const artist = { artist: { name: 'The Tragically Hip' }, info: '' };
+    const shows: Show[] = [];
+    basicShowParser(html, shows, moment());
+    const [ show ] = shows;
 
-  const venue = {
-    name: 'Emo\'s',
-    address: '2015 E. Riverside',
-    url: 'http://www.emosaustin.com'
-  };
+    const artist = { artist: { name: 'The Tragically Hip' }, info: '' };
 
-  expect(show.artists).toEqual([ artist ]);
-  expect(show.venue).toEqual(venue);
-  expect(show.name).toEqual(null);
-  expect(show.url).toEqual(null);
-  expect(show.info).toEqual('6:30pm doors, 8pm show');
+    const venue = {
+      name: 'Emo\'s',
+      address: '2015 E. Riverside',
+      url: 'http://www.emosaustin.com'
+    };
+
+    expect(show.artists).toEqual([ artist ]);
+    expect(show.venue).toEqual(venue);
+    expect(show.name).toEqual(null);
+    expect(show.url).toEqual(null);
+    expect(show.info).toEqual('6:30pm doors, 8pm show');
+
+  });
 
 });
--- packages/showlist-austin-parser/src/parsers/parseArtists.spec.ts
+++ packages/showlist-austin-parser/src/parsers/parseArtists.spec.ts
@@ -5,51 +5,56 @@ const ma = (n = '', i = '') => ({
   info: i
 });
 
-test('parseArtists: 1 artist no info', () => {
-  const s = 'Artist Name';
-  expect([ ma('Artist Name') ]).toEqual([ ...parseArtists(s) ]);
+describe('parseArtists', () => {
+
+  it('should parse 1 artist no info', () => {
+    const s = 'Artist Name';
+    expect([ ma('Artist Name') ]).toEqual([ ...parseArtists(s) ]);
+  });
+
+  it('should 1 artist 1 info', () => {
+    const s = 'Artist Name (info)';
+    expect([ ma('Artist Name', 'info') ]).toEqual([ ...parseArtists(s) ]);
+  });
+
+  it('should 1 artist, ugly info', () => {
+    const s = 'Artist Name (info, more info)';
+    expect([ ma('Artist Name', 'info, more info') ]).toEqual([...parseArtists(s)]);
+  });
+
+  it('should parse multi artists', () => {
+    const s = 'Artist 1, Artist 2';
+    expect([ma('Artist 1'), ma('Artist 2')]).toEqual([...parseArtists(s)]);
+  });
+
+  it('should parse multi artists with ugly info', () => {
+    const s = 'Artist 1, Artist 2 (some info), Artist 3 (info, more info)';
+    expect([
+      ma('Artist 1'),
+      ma('Artist 2', 'some info'),
+      ma('Artist 3', 'info, more info')
+    ]).toEqual([...parseArtists(s)]);
+  });
+
+  it('should parse multi artists with really ugly info', () => {
+    const s = 'Artist 1, Artist 2 (some info (lol)), Artist 3 (info, more info)';
+    expect([
+      ma('Artist 1'),
+      ma('Artist 2', 'some info (lol)'),
+      ma('Artist 3', 'info, more info')
+    ]).toEqual([...parseArtists(s)]);
+  });
+
+  it('should parse multi artists with really ugly info and featuring', () => {
+    const s = 'Artist 1, Artist 2 (some info (lol)), Artist 3 (info, more info), featuring Artist 4 and Artist 5';
+    expect([
+      ma('Artist 1'),
+      ma('Artist 2', 'some info (lol)'),
+      ma('Artist 3', 'info, more info'),
+      ma('Artist 4'),
+      ma('Artist 5')
+    ]).toEqual([...parseArtists(s)]);
+  });
+
 });
 
-test('parseArtists: 1 artist 1 info', () => {
-  const s = 'Artist Name (info)';
-  expect([ ma('Artist Name', 'info') ]).toEqual([ ...parseArtists(s) ]);
-});
-
-test('parseArtists: 1 artist, ugly info', () => {
-  const s = 'Artist Name (info, more info)';
-  expect([ ma('Artist Name', 'info, more info') ]).toEqual([...parseArtists(s)]);
-});
-
-test('parseArtists: multi artists', () => {
-  const s = 'Artist 1, Artist 2';
-  expect([ma('Artist 1'), ma('Artist 2')]).toEqual([...parseArtists(s)]);
-});
-
-test('parseArtists: multi artists with ugly info', () => {
-  const s = 'Artist 1, Artist 2 (some info), Artist 3 (info, more info)';
-  expect([
-    ma('Artist 1'),
-    ma('Artist 2', 'some info'),
-    ma('Artist 3', 'info, more info')
-  ]).toEqual([...parseArtists(s)]);
-});
-
-test('parseArtists: multi artists with really ugly info', () => {
-  const s = 'Artist 1, Artist 2 (some info (lol)), Artist 3 (info, more info)';
-  expect([
-    ma('Artist 1'),
-    ma('Artist 2', 'some info (lol)'),
-    ma('Artist 3', 'info, more info')
-  ]).toEqual([...parseArtists(s)]);
-});
-
-test('parseArtists: multi artists with really ugly info and featuring', () => {
-  const s = 'Artist 1, Artist 2 (some info (lol)), Artist 3 (info, more info), featuring Artist 4 and Artist 5';
-  expect([
-    ma('Artist 1'),
-    ma('Artist 2', 'some info (lol)'),
-    ma('Artist 3', 'info, more info'),
-    ma('Artist 4'),
-    ma('Artist 5')
-  ]).toEqual([...parseArtists(s)]);
-});
