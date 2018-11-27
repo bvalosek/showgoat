c4546edec5675f27a7598712143e5ff1ebad4083
Brandon Valosek <bvalosek@gmail.com>
2018-11-27 08:14:59 -0600

    service: shows endpoint loads file from s3

--- packages/showgoat-service/src/aws/s3.ts
+++ packages/showgoat-service/src/aws/s3.ts
@@ -12,12 +12,28 @@ export async function putObject(key: string, body: string): Promise<void>
 {
   const Bucket = `${PREFIX}-content`;
 
-  const params = {
-    Bucket,
-    Key: key,
-    Body: body,
-  };
+  const params = { Bucket, Key: key, Body: body, };
 
   await s3.putObject(params).promise();
+
   console.log(`put ${Bucket}/${key}`);
+}
+
+export async function getTextObject(key: string): Promise<string>
+{
+  const Bucket = `${PREFIX}-content`;
+
+  const params = { Bucket, Key: key };
+
+  const resp = await s3.getObject(params).promise();
+
+  if (!resp.Body) {
+    throw new Error();
+  }
+
+  const text = resp.Body.toString();
+
+  console.log(`fetched ${resp.ContentLength} bytes from ${Bucket}/${key}`);
+
+  return text;
 }
--- packages/showgoat-service/src/lambda/apiShows.ts
+++ packages/showgoat-service/src/lambda/apiShows.ts
@@ -1,8 +1,14 @@
 import { wrapAPI } from "./util";
 import { GetShowsAPIResponse } from "@showgoat/showgoat-common";
+import { getTextObject } from "../aws/s3";
+import { S3ShowData } from "../types";
 
 export const getShows = wrapAPI<GetShowsAPIResponse>(async () => {
+
+  const file = await getTextObject('scrapes/latest.json');
+  const data: S3ShowData = JSON.parse(file);
+
   return {
-    shows: [ ]
+    shows: data.shows
   }
 });
