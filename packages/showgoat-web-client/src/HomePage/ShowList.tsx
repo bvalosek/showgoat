4b67a151459ecb016720c052b0f1c75c7b07a9e2
jpangelle <jpangelle15@gmail.com>
2018-12-03 22:16:39 -0600

    Add ShowList component

---
+++ packages/showgoat-web-client/src/HomePage/ShowList.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { List } from 'antd';
+/* eslint-disable-next-line */
+import { Show } from '../show';
+import { GetShowsAPIResponse } from '../../../showgoat-common';
+
+export default function ShowList(props: GetShowsAPIResponse): JSX.Element {
+  const { shows } = props;
+  return (
+    <List
+      itemLayout="horizontal"
+      dataSource={shows}
+      renderItem={(item: Show): JSX.Element => (
+        <List.Item>
+          <List.Item.Meta
+            title={item.name}
+            description={item.info}
+          />
+        </List.Item>
+      )}
+    />
+  );
+}
--- packages/showgoat-web-client/src/HomePage/index.tsx
+++ packages/showgoat-web-client/src/HomePage/index.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Switch } from 'antd';
 import useFetch from '../hooks/useFetch';
 import ShowCard from './ShowCard';
+import ShowList from './ShowList';
 import { GetShowsAPIResponse } from '../../../showgoat-common';
 import './index.css';
 
 export default () => {
+  const [isCardView, setView] = useState(true);
   const [data, loading, error] = useFetch<GetShowsAPIResponse>('https://api.showgoat.net/shows');
 
   if (loading) {
@@ -24,10 +27,31 @@ export default () => {
   }
 
   return (
-    <div className="shows">
-      {data && data.shows.map(show => (
-        <ShowCard {...show} />
-      ))}
+    <div>
+      <Switch
+        checkedChildren="Card"
+        unCheckedChildren="List"
+        onChange={() => setView(!isCardView)}
+        defaultChecked
+      />
+      { isCardView
+        ? (
+          <div className="card-shows">
+            {
+              data && data.shows.map(show => (
+                <ShowCard {...show} />
+              ))
+            }
+          </div>
+        )
+        : (
+          <div className="list-shows">
+            { data && (
+              <ShowList {...data} />
+            )}
+          </div>
+        )
+      }
     </div>
   );
 };
